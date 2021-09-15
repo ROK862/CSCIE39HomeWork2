2a9bb9f45fdd9c312a895cf6792b9ad113161e88
ROBBINS JEFFERSON KARISEB <88340948+ROK862@users.noreply.github.com>
2021-09-15 20:59:07 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,16 +12,6 @@ function App() {
         <p>
           My name is Robbins Jefferson Kariseb
         </p>
-    
-        <script>
-          const APP = document.getElementsByClassName("App-header")[0];
-
-          setInterval(() => {
-            const colors = ["#b10000","#6db100","#041031","#575a63","#000000"];
-            let rndInt = Math.floor(Math.random() * colors.length) + 1;
-            if (APP) APP.style.background=colors[rndInt];
-          }, 2000);
-        </script>
       </header>
     </div>
   );
