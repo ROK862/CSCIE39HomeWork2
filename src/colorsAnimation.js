4fe6093bd2639d832430f1a0479c315a8d9ba4db
ROBBINS JEFFERSON KARISEB <88340948+ROK862@users.noreply.github.com>
2021-09-15 20:38:38 +0200

    Final Commit

--- src/colorsAnimation.js
+++ src/colorsAnimation.js
@@ -2,7 +2,6 @@ const APP = document.getElementsByClassName("App-header")[0];
 
 setInterval(() => {
 	const colors = ["#b10000","#6db100","#041031","#575a63","#000000"];
-	const rndInt = Math.floor(Math.random() * colors.length) + 1
-    	console.log(rndInt)
+	const rndInt = Math.floor(Math.random() * colors.length) + 1;
 	if (APP) APP.style.background=colors[rndInt];
 }, 2000);
