9652e20ed95a68a7c8cf94077c2d041144c983fb
ROBBINS JEFFERSON KARISEB <88340948+ROK862@users.noreply.github.com>
2021-09-15 20:35:05 +0200

    ColorsAnimation

--- src/colorsAnimation.js
+++ src/colorsAnimation.js
@@ -1,10 +1,8 @@
+const APP = document.getElementsByClassName("App-header")[0];
+
 setInterval(() => {
 	const colors = ["#b10000","#6db100","#041031","#575a63","#000000"];
-	
-	Object.keys(colors).forEach(e => {
-		if (Math.random() > 0.5) {
-			const app = document.getElementsByClassName("App-header")[0];
-			if (app){app.style.background=colors[e];console.log(colors[e]);}
-		}
-	});
+	const rndInt = Math.floor(Math.random() * colors.length) + 1
+    	console.log(rndInt)
+	if (APP) APP.style.background=colors[rndInt];
 }, 2000);
