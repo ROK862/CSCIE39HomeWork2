b614fedda228ff95e1994585c52aeb8a46803b33
ROBBINS JEFFERSON KARISEB <88340948+ROK862@users.noreply.github.com>
2021-09-15 20:28:24 +0200

    Colors Animation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './explosions.scss';
+import './colorsAnimation.js'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
